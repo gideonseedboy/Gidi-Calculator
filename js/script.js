c636e6e36a8ba3ebe2a101dbbd27952974ca717c
Ojay Tech <gideonseedboy@gmail.com>
2020-10-06 05:20:36 +0000

    Few modifications and Upload to github after 2years and a half since it was designed

---
+++ js/script.js
@@ -0,0 +1,78 @@
+document.getElementById("Butn-24").onclick = innerHTML("welcome, have a nice  time with the Calculator!");
+
+
+
+ 	userAge = prompt("How old are you Please?");
+
+
+//  	 if (userAge >= 18) {
+
+//  	 	alert('Thanks for your patience and welcome on board, you may now move on!');
+//  	 } else{
+//  	 	alert("Sorry you are not of the age permited to use this app!")
+//  	 }
+// }
+
+// document.write ("Hello World!");
+
+
+// function testApp(argument) {
+	
+// 	document.write("display-screen").innerHTML = "0.";
+// }
+
+
+
+// // var butOne = ""; 
+// //  	butOne = document.getElementById("Butn-4").onclick = function displayValue(argument) {
+
+// //  			document.write
+
+//  		 	}
+
+//  function dispValue(if (true) {}) {
+ 									
+//  }
+
+
+// We are now creating JavaScript functions for the various Applications functions of the Giddy MultiApp
+
+// First we will start with the CALCULATOR FUNCTION;
+
+// function runCal(argument) {
+
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /function clearScrn(val)
+// {
+// document.getElementById("d").value=val;
+// }
+// function v(val)
+// {
+// document.getElementById("d").value+=val;
+// }
+// document.getElementById('Butn-1').onclick = function displayValue(argument) {
+
+// 	document.write
+// }
+
+
+
+// document.getElementById("")
